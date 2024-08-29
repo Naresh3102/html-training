9800b5e65a53e32c732b4741c37bf97aff322dad
Naresh3102 <naresh31122002@gmail.com>
2024-08-29 19:03:22 +0530

    Day10

---
+++ Day10/index.js
@@ -0,0 +1,23 @@
+var firstName = "naresh";
+let city = "Erode";
+const skills = "MERN";
+let day = 10;
+let boolVar = true;
+let floatVar = 10.5;
+
+// skills = "Java"
+
+console.log(day);
+
+firstName = "esvar"
+day = 11
+
+// console.log(typeof(firstName));
+// console.log(typeof(day));
+// console.log(typeof(boolVar));
+// console.log(typeof(floatVar));
+
+console.log(firstName);
+console.log(day);
+
+
