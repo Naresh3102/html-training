228335ffa84bc3e52cf338dd59a467e6c6ece93d
Naresh3102 <naresh31122002@gmail.com>
2024-08-30 19:13:00 +0530

    Day11

---
+++ Day11/script.js
@@ -0,0 +1,114 @@
+// // Re-declaration
+
+// // Not recommended
+// var a = 1;
+// var a = 2;
+
+// let b;
+
+// const c = 1;
+
+// console.log(a);
+// console.log(b);
+// b = 2
+// console.log(b);
+// console.log(c);
+
+// // Scope
+
+// let a;
+
+// {
+//     a = 10;
+//     const b = 20;
+
+//     console.log(a);
+//     console.log(b);
+// }
+
+// console.log(a);
+// console.log(b);
+
+// console.log(Number.MAX_VALUE);
+
+// let bigIntNumber = 123456789012345678912345678901234567890n;
+
+// let firstName = null;
+
+// console.log(typeof(2))
+
+// Arithmetic Operators
+
+// let a = 10;
+// let b = 20;
+
+// let add = a + b;
+
+// console.log(add);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(10 / 5);
+
+// console.log(a % b);
+// console.log(6 % 5);
+
+// console.log('--------------------');
+
+// a--
+
+// let c = a
+// console.log(c);
+// console.log(a);
+// console.log(c);
+
+// console.log(2 ** 8);
+
+// Assignment Operator
+
+// let a = 10;
+// let b = 20;
+
+// // a = a + 2
+// // a += 2
+// // a = a - 2
+// // a-=2
+// // a = a * 2
+// // a*=2
+// // a = a / 2
+// // a/=2
+// // a = a % 3
+// // a%=3
+// // a = a ** 2
+// a**=2
+
+// console.log(a);
+
+// Comparison operators
+
+// console.log(1 == 1);
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log(1 === 1);
+// console.log("hi" == "hi");
+// console.log("hi" === "hi");
+
+// console.log("-----------------------");
+
+// console.log(1 != 1);
+// console.log(1 !== "1");
+
+// console.log("------------------------");
+
+// console.log(1 > 3);
+// console.log(3 > 1);
+
+// console.log(1 < 3);
+// console.log(3 < 1);
+
+// console.log(1 <= 1);
+// console.log(3 >= 3);
+
+console.log((1 == 1) || (1 != 1)); // true && false
+
+console.log(!(1 != 1));
